d0f53d10a9dc9305a66f029f8d91419c4b71492a
bobrov.dmitriy <bobrov.dmitriy@mail.ru>
2023-12-07 15:22:20 +0300

    Add com/hid lists

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,8 @@
 #include <iostream>
-#include <boost/filesystem.hpp>
-#include <boost/asio/impl/src.hpp>
 #include <boost/regex.hpp>
+#include <boost/asio.hpp>
+
 
 int main() {
-    using boost::asio;
-    using namespace boost::filesystem;
-    path p{"C:\\.conan\\"};
-  try
-  {
-    file_status s = status(p);
-    std::cout << std::boolalpha << is_directory(s) << '\n';
-  }
-  catch (filesystem_error &e)
-  {
-    std::cerr << e.what() << '\n';
-  }
-    std::cout<<"Success!"<<std::endl;
-
-// io_service service;
-// ip::tcp::endpoint ep( ip::address::from_string("127.0.0.1"), 2001);
-// ip::tcp::socket sock(service);
-// sock.connect(ep);
-
-
- std::string s = "Boost Libraries";
-  boost::regex expr{"\\w+\\s\\w+"};
-  std::cout << std::boolalpha << boost::regex_match(s, expr) << '\n';
-    return 0;
+  
 }
