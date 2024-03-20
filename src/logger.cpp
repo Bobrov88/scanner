ece11a887bce0147fd9947e8b947f163a423fd2f
bobrov.dmitriy <bobrov.dmitriy@mail.ru>
2024-03-20 09:32:00 +0200

    Boost.Log full solved

---
+++ src/logger.cpp
@@ -0,0 +1,30 @@
+#include "logger.h"
+
+BOOST_LOG_GLOBAL_LOGGER_INIT(my_logger, lg_type)
+{
+
+    lg_type lg;
+
+    typedef sinks::synchronous_sink<sinks::text_file_backend> file_sink;
+    boost::shared_ptr<file_sink> f_sink(new file_sink(
+        keywords::file_name = "scan_log.log",
+        keywords::open_mode = std::ios_base::app,
+        keywords::auto_flush = true,
+        keywords::max_size = 10 * 1024 * 1024));
+    f_sink->set_filter(expr::attr<int>("Severity") == 1);
+    logging::formatter formatter = expr::stream << "["
+                                                << expr::attr<boost::posix_time::ptime>("TimeStamp")
+                                                << "] -> "
+                                                << expr::smessage;
+    f_sink->set_formatter(formatter);
+    logging::core::get()->add_sink(f_sink);
+
+    boost::shared_ptr<sinks::synchronous_sink<sinks::text_ostream_backend>> c_sink = logging::add_console_log();
+    c_sink->set_filter(expr::attr<int>("Severity") == 0);
+    c_sink->locked_backend()->auto_flush(true);
+    logging::core::get()->add_sink(c_sink);
+
+    logging::core::get()->add_global_attribute("TimeStamp", attrs::utc_clock());
+
+    return lg;
+}
